24f33f40dbce189e3fac32e41beabde95ae09cd9
Lautaro Appelhans <141765194+Lautiapp@users.noreply.github.com>
2025-10-05 22:57:18 -0300

    Revert "Master"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,23 @@
-import { useState } from 'react';
-import { CartProvider } from './components/CartContext';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Inicio from './secciones/Inicio';
-import Catalogo from './pages/Catalogo';
-import Contacto from './secciones/Contacto';
-import Carrito from './secciones/Carrito';
-import DetalleProducto from './secciones/DetalleProducto';
-import './App.css';
+import Catalogo from "./pages/Catalogo.jsx";
 
 function App() {
-  const [paginaActual, setPaginaActual] = useState('inicio');
-  const [productoSeleccionado, setProductoSeleccionado] = useState(null);
-
-  const cambiarPagina = (pagina, productoId = null) => {
-    setPaginaActual(pagina);
-    if (productoId) {
-      setProductoSeleccionado(productoId);
-    }
-  };
-
-  let paginaAMostrar;
-
-  if (paginaActual === 'inicio') {
-    paginaAMostrar = <Inicio cambiarPagina={cambiarPagina} />;
-  } else if (paginaActual === 'catalogo') {
-    paginaAMostrar = <Catalogo cambiarPagina={cambiarPagina} />;
-  } else if (paginaActual === 'contacto') {
-    paginaAMostrar = <Contacto />;
-  } else if (paginaActual === 'carrito') {
-    paginaAMostrar = <Carrito cambiarPagina={cambiarPagina} />;
-  } else if (paginaActual === 'detalle') {
-    paginaAMostrar = <DetalleProducto productoId={productoSeleccionado} cambiarPagina={cambiarPagina} />;
-  } else {
-    paginaAMostrar = <Inicio cambiarPagina={cambiarPagina} />;
-  }
-
   return (
-    <CartProvider>
-      <div className="App">
-        <Header cambiarPagina={cambiarPagina} paginaActual={paginaActual} />
-        {paginaAMostrar}
-        <Footer cambiarPagina={cambiarPagina} />
-      </div>
-    </CartProvider>
+    <div>
+      <header>
+        <h1>Mi Tienda</h1>
+      </header>
+
+      {/* Páginas */}
+      <Catalogo />
+
+      <footer>
+        <p>© 2025 Mi Tienda</p>
+      </footer>
+    </div>
   );
 }
 
 export default App;
+
+
+
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,10 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import App from './App.jsx'
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<App />)
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
