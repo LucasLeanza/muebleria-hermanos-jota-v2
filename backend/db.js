c87c5b239927cdc0fb7946fa4c67057cf6b5f18e
LucasLeanza <lucasagustinleanza@gmail.com>
2025-11-02 20:13:55 -0300

    Fix de variable MONGO_URI en db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('Conectado a la base de datos: ' + mongoose.connection.db.databaseName);
     } catch (error) {
         console.log('no se ha podido conectar: ' + error.message);
