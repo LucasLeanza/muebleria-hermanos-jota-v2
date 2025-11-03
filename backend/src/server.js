e23e85d2d07e94b82f548102bbd1761521938bdc
Lautiapp <lautiapp@gmail.com>
2025-11-03 01:10:19 -0300

    Fix para deployear en Netlify y Render

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
+import 'dotenv/config'  
 import app from './app.js' 
 import connectDB from '../db.js'       
-import 'dotenv/config'                
+              
 
 const PORT = process.env.PORT || 3000
 
